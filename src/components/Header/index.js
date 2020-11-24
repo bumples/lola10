d0d89c661894ec5112d7045dc209f8c623937ad6
Olieb2020 <bumples@aol.com>
2020-11-24 15:13:14 -0500

    commit11

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { HeaderWrapper } from './styles';
 import { Cart } from '../Cart';
-import { Logo } from '../Logo';
+import {Logo} from '../Logo';
 import { Search } from '../Search';
 import { Link } from 'gatsby';
 
 export function Header() {
   return (
     <HeaderWrapper>
-      <div>
-        <Link to="/">
-          <Logo />
-        </Link>
-      </div>
+      <Link to="/">
+        <Logo/>
+      </Link>
       <Search />
       <Cart />
     </HeaderWrapper>
