53f258ad0140c9b128fc4ce08e24eb48da6a39b9
Olieb2020 <bumples@aol.com>
2021-02-10 11:17:05 -0500

    final8

--- src/components/CartContents/index.js
+++ src/components/CartContents/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import CartContext from 'context/CartContext';
-import { CartItem, CartHeader, CartFooter, Footer } from './styles';
-import { QuantityAdjuster } from '../QuantityAdjuster';
-import { RemoveLineItem } from '../RemoveLineItem';
-import { Button } from '../Button';
-import { navigate } from '@reach/router';
+import { CartHeader  } from './styles';
 
 
 export function CartContents() {
@@ -26,53 +22,9 @@ return (
         </CartHeader>
     )}
 
-    {checkout?.lineItems?.map(item => (
-        <CartItem key={item.variant.id}>
-    <div>
-            <div>{item.title}</div>
-            <div>
-            {item.variant.title === 'Default Title' ? '' : item.variant.title}
-            </div>
-    </div>
-        <div>£{item.variant.price}</div>
-        <div>
-            <QuantityAdjuster item={item} onAdjust={handleAdjustQuantity} />
-        </div>
-          <div>£{(item.quantity * item.variant.price).toFixed(2)}</div>
-        <div>
-            <RemoveLineItem lineItemId={item.id} />
-        </div>
-        </CartItem>
-    ))}
-{!!checkout?.lineItems && (
-        <CartFooter>
-    <div>
-            <strong>Total:</strong>
-    </div>
-    <div>
-            <span>£{checkout?.totalPrice}</span>
-    </div>
-        </CartFooter>
-    )}
-{!checkout?.lineItems && <h4>You cart is empty.</h4>}
-<Footer>
-        <div>
-    <Button onClick={() => navigate(-1)}>Continue shopping</Button>
-        </div>
-        <div>
-    {!!checkout?.webUrl && (
-            <Button
-        onClick={() => {
-                window.location.href = checkout.webUrl;
-    }}
-            >
-        Checkout
-            </Button>
-    )}
-        </div>
-    </Footer>
+   
 </section>
 );
 }
 
-export * from './CartContents';
+//export * from './CartContents';//
